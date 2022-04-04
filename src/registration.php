22d4b8c83bbdb372d63e418bb65c0e569aaa4a7a
Marius <marius@coddin.nl>
2022-04-04 08:40:07 +0200

    Ignore registration.php for coverage

--- src/registration.php
+++ src/registration.php
@@ -4,11 +4,10 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-/**
- * @codeCoverageIgnore
- */
+// @codeCoverageIgnoreStart
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'Coddin_CartBridge',
     __DIR__
 );
+// @codeCoverageIgnoreEnd
